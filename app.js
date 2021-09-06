425292313a9473f38da36400c92c63d508b6b76e
nishasen <43262505+nishasen@users.noreply.github.com>
2021-09-06 14:53:34 +0530

    Completed

---
+++ app.js
@@ -0,0 +1,41 @@
+const billAmount = document.querySelector("#bill-amount")
+// const btnNext = document.querySelector("#btn-next")
+const cashGiven = document.querySelector("#cash-given")
+const btnCheck = document.querySelector("#btn-check")
+const errorMessage = document.querySelector("#error-message")
+const noOfNotes = document.querySelectorAll(".no-of-notes")
+
+const availableNotes = [2000, 500, 100, 20, 10, 5, 1]
+
+btnCheck.addEventListener("click", function validateBillAndCashAmount() {
+    hideMessage();
+    if(billAmount.value > 0) {
+        if(cashGiven.value >= billAmount.value) {
+            const amountToBeReturned = cashGiven.value - billAmount.value;
+            calculateChange(amountToBeReturned);
+        } else {
+            showMessage("Do you wanna wash plates?");
+        }
+    } else {
+       showMessage("Bill amount is invalid");
+    }
+});
+
+function hideMessage() {
+    errorMessage.style.display = "none";
+}
+
+function calculateChange(amountToBeReturned){
+    for(let i=0;i<availableNotes.length;i++) {
+        const numberOfnOtes = Math.trunc(
+            amountToBeReturned / availableNotes[i]
+        );
+        amountToBeReturned %= availableNotes[i];
+        noOfNotes[i].innerText = numberOfnOtes;
+    }
+}
+
+function showMessage(msg) {
+    errorMessage.style.display = "block";
+    errorMessage.innerText = msg;
+}
